bd0ba7d51884355d5a8bc80c6a21ccbbff3289a8
jaypokharna <pokharnajay@gmail.com>
2023-12-23 19:21:59 +0530

    Learned about node and npm

---
+++ index.js
@@ -0,0 +1,19 @@
+const http = require("http")
+
+const server = http.createServer((req , res)=>{
+
+    if(req.url === "/about") {
+        res.end("<h1>About</h1>")
+    }
+    if(req.url === "/") {
+        res.end("<h1>Home</h1>")
+    }
+    else{
+        res.end("<h1>404 - Page not found</h1>")
+    }
+
+});
+
+server.listen(5000,()=>{
+    console.log("Server is working") 
+});
---
+++ script.js
@@ -0,0 +1,10 @@
+var fig = require('figlet');
+
+fig("jay", function (err, data) {
+    if (err) {
+        console.log("Something went wrong...");
+        console.dir(err);
+        return;
+    }
+    console.log(data);
+});
